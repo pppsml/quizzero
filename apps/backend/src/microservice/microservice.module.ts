9fc7256aad6cefac562ef5ce5bac81ebc3e03214
Vladislav Shumkin <vldslv.shumkin@gmail.com>
2024-07-09 13:08:34 +0500

    not worked

---
+++ apps/backend/src/microservice/microservice.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+
+import { MicroserviceController } from "./microservice.controller";
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ACCOUNT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'account_service_queue',
+        }
+      }
+    ]),
+  ],
+  controllers: [MicroserviceController],
+})
+export class MicroserviceModule {}
